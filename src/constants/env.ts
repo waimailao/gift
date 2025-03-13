890c11b0720985b59e1f80d017c89b7c7c10eb5c
waimailao <wangfurenailaoyang@163.com>
2025-03-13 14:41:56 +0800

    fix

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -2,5 +2,5 @@ export const ENV = {
   USER_PRIVACY_POLICY: 'https://baidu.com',
   COIN_COLLECT_HEIGHT: 42,
   SENTRY_KEY: 'AAE3JRwvzYlXvEvmGgHT3IP8TGGsnnkmPCc',
-  API_CLIENT_URL: 'http://gift-api-test.tele.gifts',
+  API_CLIENT_URL: 'https://gift-api-test.tele.gifts',
 }
